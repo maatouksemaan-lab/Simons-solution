7ea6dbc70d7faa7df4597ec21be2cd54aabcdf18
maatouksemaan-lab <maatouksemaan@gmail.com>
2026-09-04 13:14:24 +0300

    Delete Index.tsx

--- Index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Home() {
-  return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#000'}}>
-      <Text style={{color:'white', fontSize:24}}>Simons Solution</Text>
-    </View>
-  );
-}
