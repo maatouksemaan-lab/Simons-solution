c7b33c9adc1ef3db9cdf803662975ef55ed860fa
maatouksemaan-lab <maatouksemaan@gmail.com>
2026-09-04 13:09:38 +0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+import { Text, View } from 'react-native';
+export default function App() {
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'#000'}}>
+      <Text style={{color:'white', fontSize:24}}>Simons Solution</Text>
+    </View>
+  );
+}
